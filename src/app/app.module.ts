0b8ff912c7e86c7e7974720df83c7c1c1a53ae08
Nour Alhadi Mahmoud <nour.alhadi.99@gmail.com>
2019-06-25 20:36:47 +0300

    Added Components & Active Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    SearchComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
